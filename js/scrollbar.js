0be98a000be2cfa7589fd0c5c7878ba8be852a61
lazygeniuslanz <lanz.irlanda@gmail.com>
2018-09-06 16:25:51 +0800

    Add files via upload

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,14 +1,14 @@
 
 $(document).ready(function() {
-	$('.scrollbar').fadeOut(250);
+	//$('.scrollbar').fadeOut(250);
 });
 
 $(window).scroll(function() {
-    $('.scrollbar').fadeIn(250);
+    $('.scrollbar').fadeIn(400);
     
     clearTimeout($.data(this, "scrollTimer"));
     $.data(this, 'scrollTimer', setTimeout(function() {
-        $('.scrollbar').fadeOut(500);
+        $('.scrollbar').fadeOut(250);
         console.log("Haven't scrolled in 2000ms!");
     }, 1500));
 });
