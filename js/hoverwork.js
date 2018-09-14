edc706cae0bbc6b096457172b6c8cfd7fcd10cba
lazygeniuslanz <lanz.irlanda@gmail.com>
2018-09-14 10:36:35 +0800

    update

--- js/hoverwork.js
+++ js/hoverwork.js
@@ -1,14 +1,34 @@
 var currentid = "";
-function a(id){
+function loadwork(id){
     
     if(currentid != id){
         currentid = id;
         
-        console.log(id);
-        var x = $("#" + id).find($("img")).attr("src");
-        console.log(x);
-        
-        $(".works-container").css("background-image", "url(" + x + ")")
+        ChangeWorkBackground(id);
     }
     
 }
+
+function ChangeWorkBackground(id){
+    var src = $("#" + id).find($("img")).attr("src");
+    $(".works-container").css("background-image", "url(" + src + ")")
+}
+
+var isMobile = false;
+$(document).ready(function(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        isMobile = true;
+     $(".works-container").css("background-image", "url()");
+     $(".work").css('opacity', '1');
+    }
+
+    $('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+$(window).scroll(function(){
+    if(isMobile){
+        $(".work").css('opacity', '1');
+    }
+})
